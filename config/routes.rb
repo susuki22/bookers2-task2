a20b492d99bf9dd8326e44ec1cd9b7428dc60768
susuki22 <shoya.s.0427@email.com>
2021-11-02 07:32:18 +0000

    [Start]6-3

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :favorites, only: [:create, :destroy]
 end
